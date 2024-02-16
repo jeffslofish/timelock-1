203e7fd72741788ad29ebf9015a3699e4fe3231d
AceWayne <agentacewayne@gmail.com>
2024-02-15 20:57:22 -0500

    Sizing changes

--- src/main/main.ts
+++ src/main/main.ts
@@ -128,8 +128,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1416,
+    height: 948,
     //resizable: false,
     icon: getAssetPath('icon.png'),
     webPreferences: {
